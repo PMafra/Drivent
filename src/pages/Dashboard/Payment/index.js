a8e89ae14a7addad4cca57c0ef3209241fe7becc
Ben Hur Faria <benhurfaria3@gmail.com>
2022-01-21 01:12:32 -0300

    Add buttons modal tickets

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,17 +1,29 @@
 import useApi from "../../../hooks/useApi";
 import Unauthorized from "../../../components/shared/Unauthorized";
+import React, { useEffect, useState } from "react";
+import Tickets from "../../../components/Payment/Tickets";
 
 export default function Payment() {
   const { enrollment } = useApi();
 
-  enrollment.getPersonalInformations().then((resp) => {
-    console.log(resp.data);
-  });
+  const [subscription, setSubscription] = useState(false);
   
+  useEffect(() => {
+    enrollment.getPersonalInformations().then((resp) => {
+      if (resp.data.id) setSubscription(!subscription);
+    });
+  }, []);
+
   return (
-    <Unauthorized
-      message="Você precisa completar sua inscrição antes
+    <>
+      {!subscription ? (
+        <Unauthorized
+          message="Você precisa completar sua inscrição antes
   de prosseguir pra escolha de ingresso"
-    />
+        />
+      ) : (
+        <Tickets />
+      )}
+    </>
   );
 }
