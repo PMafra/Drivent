00163e1ccddee9b0d0ffcc3b2b8123a9c4dd44fe
victordurco <victordurco@gmail.com>
2022-02-02 16:03:39 -0300

    certificate component done

--- src/components/Certificate/index.js
+++ src/components/Certificate/index.js
@@ -1,15 +1,28 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 
-export default function UserCertificate() {
+export default function UserCertificate({ ticketInfo, userInfo }) {
   const EVENT_HOURS = 15;
+  const [modalityName, setModalityName] = useState("");
+
+  function cleanModalityName() {
+    const modalityName = ticketInfo.modality.name.split("+");
+    return modalityName[0];
+  }
+
+  useEffect(() => {
+    setModalityName(cleanModalityName());
+  }, []);
+
   return (
     <Container>
       <Title>Certificado</Title>
-      <Text>Certificamos que Victor Durço Gomes Bijos concluiu o evento de tecnologia Drivent, com duração de {EVENT_HOURS} horas entre os dias 22/10, 23/10 e 24/10.
+      <Text>Certificamos que {userInfo.name} concluiu o evento de tecnologia Drivent, com duração de {EVENT_HOURS} horas entre os dias 22/10, 23/10 e 24/10.
         <br/>
-      Tipo de participação: presencial.  
+      Tipo de participação:  {modalityName}.  
       </Text>
       <Signature>Equipe Drivent</Signature>
+      _________________________________
     </Container>
   );
 }
@@ -17,7 +30,7 @@ export default function UserCertificate() {
 const Container = styled.div`
     width: 650px;
     height: 450px;
-    background-color: #ededed;
+    background-image: linear-gradient(to right, #ee409740, #f8d87f49);
     border-radius: 5px;
     display: flex;
     flex-direction: column;
@@ -32,13 +45,15 @@ const Title = styled.span`
 
 const Text = styled.p`
   width: 85%;
-  font-size: 15px;
+  font-size: 18px;
   text-align: center;
   margin: 80px 0;
   line-height: 30px;
 `;
 
 const Signature = styled.span`
-  font-size: 25px;
-  margin-top: 80px;
+  font-size: 30px;
+  margin-top: 40px;
+  font-weight: bold;
+  font-family: 'Oooh Baby', cursive;
 `;
--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,13 +1,54 @@
 import styled from "styled-components";
 import Typography from "@material-ui/core/Typography";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
+import useApi from "../../../hooks/useApi";
 
 import UserCertificate from "../../../components/Certificate";
+import Unauthorized from "../../../components/shared/Unauthorized";
 
 export default function Certificate() {
+  const { enrollment, ticket } = useApi();
+  const [userInfo, setUserInfo] = useState("");
+  const [ticketInfo, setTicketInfo] = useState("");
+  const [authorized, setAuthorized] = useState(false);
+ 
+  useEffect(() => {
+    ticket.getTicketInformations().then((res) => {
+      setTicketInfo(res.data[0]);
+    }).catch((err) => {
+      toast("Houve um problema ao buscar as informações do ticket");
+    });
+
+    enrollment.getPersonalInformations().then((res) => {
+      setUserInfo(res.data);
+    }).catch((err) => {
+      toast("Houve um problema ao buscar as informações do usuário");
+    });
+  }, []);
+
+  useEffect(() => {
+    if (userInfo && ticketInfo && ticketInfo.isPaid) {
+      setAuthorized(true);
+    }
+  }, [userInfo, ticketInfo]);
+
   return (
     <>
-      <Title variant="h4">Seu certificado</Title>
-      < UserCertificate />
+      { authorized
+        ? (<>
+          <Title variant="h4">Seu certificado</Title>
+          < UserCertificate 
+            userInfo = {userInfo}
+            ticketInfo = {ticketInfo}
+          />
+        </>)
+        : (
+          < Unauthorized  message = "Certificado não disponível"/>
+        )
+      }
+     
     </>
   );
 }
