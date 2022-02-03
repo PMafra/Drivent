ce625d981c7fc9ca807506af92513601c5f1072c
leandrodcs <eng.leandrodcs@gmail.com>
2022-02-02 22:18:18 -0300

    Download certificate button added

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -2,9 +2,6 @@ import Typography from "@material-ui/core/Typography";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import styled from "styled-components";
-import { Button } from "@material-ui/core";
-import * as htmlToImage from "html-to-image";
-import jsPDF from "jspdf";
 
 import useApi from "../../../hooks/useApi";
 
@@ -16,23 +13,6 @@ export default function Certificate() {
   const [userInfo, setUserInfo] = useState("");
   const [ticketInfo, setTicketInfo] = useState("");
   const [authorized, setAuthorized] = useState(false);
-
-  function downloadPdf() {
-    htmlToImage.toPng(document.getElementById("pdf"))
-      .then(dataUrl => {
-        const link = document.createElement("a");
-        link.download = "Certificado.png";
-        const doc = new jsPDF({
-          orientation: "l",
-          unit: "pt",
-          format: "a4",
-        });
-        const pdfWidth = doc.internal.pageSize.getWidth();
-        let pdfHeight = doc.internal.pageSize.getHeight();
-        doc.addImage(dataUrl, "PNG", 0, 0, pdfWidth, pdfHeight);
-        doc.save("Certificado.pdf"); 
-      });
-  }
  
   useEffect(() => {
     ticket.getTicketInformations().then((res) => {
@@ -64,7 +44,6 @@ export default function Certificate() {
             userInfo = {userInfo}
             ticketInfo = {ticketInfo}
           />
-          <DownloadPdfButton onClick={downloadPdf}>DOWNLOAD DO CERFITICADO</DownloadPdfButton>
         </Wrapper>
         : 
         < Unauthorized  message = "Certificado não disponível"/>
@@ -80,12 +59,4 @@ const Title = styled(Typography)`
 
 const Wrapper = styled.section`
   height: 100%;
-`;
-
-const DownloadPdfButton = styled(Button)`
-  width: 250px;
-  height: 45px;
-  border: 1px solid #CECECE !important;
-  font-weight: 700 !important;
-  border-radius: 10px !important;
 `;
